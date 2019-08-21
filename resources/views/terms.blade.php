c751e43b2f949a36868ef5305c37886e661a6f39
Joyce <joycenpotestades@gmail.com>
2019-08-21 22:39:23 +0800

    yey

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -7,15 +7,19 @@
   <body style="padding:20px;color: #2b2b2b;">
     <h1 style="text-align:center;color:#388e3c!important">PRIVACY AND TERMS</h1>
     <h3 style="text-align:center">To create a WATSAP Account, you’ll need to agree to the Terms of Service below.</h3>
-    <h4>In addition, when you create an account, we process your information as described in our Privacy Policy, including these key points:</h4>
-    <ul>
-      <li>Data we process when you use WATSAP</li>
-      <li>When you set up a WATSAP account, we store information you give us like your name, email address, and telephone number.</li>
-      <li>When you use WATSAP services to do selling product. We process information about that activity.</li>
-    </ul>
-    <h4>We process this data for the purposes described in our policy</h4>
-    <h4>Improve security by protecting against fraud and abuse.</h4>
-    <h4>You’re in control</h4>
-    <h4>We treat this data as personal information.</h4>
+    <p>The data we collect from users will be stored in the identification photos in our database. Personal Data refers to any information that enables WATSAP to identify you as a user. In addition, when you create an account, we process your information as described below in our Privacy Policy.</p>
+    <h3>How WATSAP Collects Data and Use Your Personal Data?</h3>
+    <h4>Personal Data Collection for Becoming A Seller</h4>
+    <p>Before using WATSAP, you may need to provide Personal Data. In case you do not provide certain personal data, we will not be able to provide you access to becoming a seller.</p>
+    <h4>Personal data that you provide to WATSAP</h4>
+    <p>     You need to register a WATSAP account to enjoy certain functions and services. When you register by filling up the form with your name, email address and mobile number then and Log in to WATSAP to buy online and if you want to become a seller, we will ask you to provide personal data such as government issued IDs.</p>
+    <h4>How WASAP Protects Your Personal Data?</h4>
+    <p>     We attach great importance to the security of your Personal Data and have adopted standard industry practices to protect your Personal Data and prevent it from unauthorized access, disclosure, use, modification, damage or loss. To this end, we take the following measures:</p>
+    <ol>
+      <li>We take reasonable and feasible measures to ensure that the personal data collected is minimal and relevant to what is necessary in relation to the purposes for which they processed. We retain the customer personal data for the duration of his relationship. When you delete your WATSAP account, personal data to your account such as your name, email address and mobile number will be erased.</li>
+      <li>We implement trusted protection mechanisms to protect data and data storage from attacks, fraud or abuse</li>
+      <li>We deploy access control mechanisms to ensure that only authorized personnel can access your personal data.</li>
+      <li>We treat the data as privacy and strictly not shareable to unauthorized people.</li>
+    </ol>
   </body>
 </html>
